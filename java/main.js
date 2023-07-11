9c944fac8a2fc102bc9f113bcc26f10c43358632
Exo-MDR-CD2000 <joseguillen587@yahoo.com>
2023-07-11 15:46:59 -0700

    added 10 js questions

--- java/main.js
+++ java/main.js
@@ -6,17 +6,128 @@
 
 
 // quiz questions
+// for reference: [a=0, b=1, c=2, d=3,]
 var questions = [
     {
-
+      question: "What is the correct syntax for declaring a variable in JavaScript?",
+      choices: [
+        "a) var x;",
+        "b) x = 5;",
+        "c) variable x;",
+        "d) int x;"
+      ],
+      answer: 0
     },
     {
-
+      question: "How do you write \"Hello, World!\" in an alert box?",
+      choices: [
+        "a) console.log(\"Hello, World!\");",
+        "b) alertBox(\"Hello, World!\");",
+        "c) alert(\"Hello, World!\");",
+        "d) document.write(\"Hello, World!\");"
+      ],
+      answer: 2
     },
     {
-
+      question: "What is the correct way to write a JavaScript comment?",
+      choices: [
+        "a)",
+        "b) //This is a comment",
+        "c) /This is a comment/",
+        "d) \\This is a comment"
+      ],
+      answer: 1
     },
     {
-
+      question: "How do you declare a function in JavaScript?",
+      choices: [
+        "a) function myFunction()",
+        "b) function = myFunction()",
+        "c) var myFunction = function()",
+        "d) def myFunction()"
+      ],
+      answer: 0
+    },
+    {
+      question: "How do you call a function named \"myFunction\"?",
+      choices: [
+        "a) call myFunction()",
+        "b) myFunction()",
+        "c) execute myFunction()",
+        "d) run myFunction()"
+      ],
+      answer: 1
+    },
+    {
+      question: "Which operator is used to assign a value to a variable in JavaScript?",
+      choices: [
+        "a) =",
+        "b) ==",
+        "c) :=",
+        "d) =>"
+      ],
+      answer: 0
+    },
+    {
+      question: "What is the correct way to write a conditional statement in JavaScript?",
+      choices: [
+        "a) if x = 5 then",
+        "b) if x == 5",
+        "c) if (x = 5)",
+        "d) if (x == 5)"
+      ],
+      answer: 3
+    },
+    {
+      question: "How do you concatenate two strings in JavaScript?",
+      choices: [
+        "a) string1 + string2",
+        "b) string1 . string2",
+        "c) string1 & string2",
+        "d) string1 ~ string2"
+      ],
+      answer: 0
+    },
+    {
+      question: "How do you write a for loop in JavaScript?",
+      choices: [
+        "a) loop(x=0; x<5; x++)",
+        "b) for (x=0; x<5; x++)",
+        "c) for loop(x=0; x<5; x++)",
+        "d) iterate(x=0; x<5; x++)"
+      ],
+      answer: 1
+    },
+    {
+      question: "What function is used to convert a string to a number in JavaScript?",
+      choices: [
+        "a) convertStringToNumber()",
+        "b) parseInt()",
+        "c) stringToNumber()",
+        "d) toNumber()"
+      ],
+      answer: 1
     }
-];
+  ];
+  
+
+
+// What is the correct syntax for declaring a variable in JavaScript? a) var x; b) x = 5; c) variable x; d) int x; Correct answer: a
+
+// How do you write "Hello, World!" in an alert box? a) console.log("Hello, World!"); b) alertBox("Hello, World!"); c) alert("Hello, World!"); d) document.write("Hello, World!"); Correct answer: c
+
+// What is the correct way to write a JavaScript comment? a) b) //This is a comment c) /This is a comment/ d) \This is a comment Correct answer: b
+
+// How do you declare a function in JavaScript? a) function myFunction() b) function = myFunction() c) var myFunction = function() d) def myFunction() Correct answer: a
+
+// How do you call a function named "myFunction"? a) call myFunction() b) myFunction() c) execute myFunction() d) run myFunction() Correct answer: b
+
+// Which operator is used to assign a value to a variable in JavaScript? a) = b) == c) := d) => Correct answer: a
+
+// What is the correct way to write a conditional statement in JavaScript? a) if x = 5 then b) if x == 5 c) if (x = 5) d) if (x == 5) Correct answer: d
+
+// How do you concatenate two strings in JavaScript? a) string1 + string2 b) string1 . string2 c) string1 & string2 d) string1 ~ string2 Correct answer: a
+
+// How do you write a for loop in JavaScript? a) loop(x=0; x<5; x++) b) for (x=0; x<5; x++) c) for loop(x=0; x<5; x++) d) iterate(x=0; x<5; x++) Correct answer: b
+
+// What function is used to convert a string to a number in JavaScript? a) convertStringToNumber() b) parseInt() c) stringToNumber() d) toNumber() Correct answer: b
